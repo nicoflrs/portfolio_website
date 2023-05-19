e058dbeb6e444de848b323e00f297a1bd61e0e04
Nico <floresni1996@gmail.com>
2023-05-18 20:39:26 -0700

    minor updates to contact info

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -20,12 +20,13 @@ const styles = createStyles({
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
-    flexDirection: "column",
   },
   heading: {
     color: "white",
     fontSize: "70px",
     fontWeight: 700,
+    display: "flex",
+    justifyContent: "center",
   },
   paragraph: {
     color: "white",
@@ -48,41 +49,48 @@ const styles = createStyles({
     paddingLeft: "64px",
     paddingRight: "64px",
   },
+  contactContainer: {
+    width: "750px",
+  },
 });
 
 interface Props extends WithStyles<typeof styles> {}
 
 const Contact: React.FC<Props> = ({ classes }) => (
   <div id="contact" className={classes.contact}>
-    <Typography className={classes.heading}>{LETS_CONNECT}</Typography>
-    <Typography className={classes.paragraph}>{CONTACT_DESCRIPTION}</Typography>
-    <div className={classes.links}>
-      <div>
-        <a
-          href="https://github.com/nicoflrs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={githubContact}></img>
-        </a>
-      </div>
-      <div className={classes.iconContainer}>
-        <a
-          href="mailto:floresni1996@gmail.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={gmail} width={74} height={74}></img>
-        </a>
-      </div>
-      <div>
-        <a
-          href="https://www.linkedin.com/in/nicolasaflores/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={linkedin} width={74} height={74}></img>
-        </a>
+    <div className={classes.contactContainer}>
+      <Typography className={classes.heading}>{LETS_CONNECT}</Typography>
+      <Typography className={classes.paragraph}>
+        {CONTACT_DESCRIPTION}
+      </Typography>
+      <div className={classes.links}>
+        <div>
+          <a
+            href="https://github.com/nicoflrs"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={githubContact}></img>
+          </a>
+        </div>
+        <div className={classes.iconContainer}>
+          <a
+            href="mailto:floresni1996@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={gmail} width={74} height={74}></img>
+          </a>
+        </div>
+        <div>
+          <a
+            href="https://www.linkedin.com/in/nicolasaflores/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={linkedin} width={74} height={74}></img>
+          </a>
+        </div>
       </div>
     </div>
   </div>
